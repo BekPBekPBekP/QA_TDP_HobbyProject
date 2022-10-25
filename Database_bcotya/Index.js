616c1547dab159aca0960e04faff1523329b46f5
BekPBekPBekP <rspalethorpe@gmail.com>
2022-10-25 14:50:44 +0100

    database express routes p2

--- Database_bcotya/Index.js
+++ Database_bcotya/Index.js
@@ -4,10 +4,14 @@ const app = express();
 
 const bodyParser = require("body-parser")
 
-const catRoutes = require("../Database_bcotya/Routes/CatRoutes");
+const catRoutes = require("./Routes/CatRoutes");
 
 app.use(bodyParser.json())
 
 app.use("/bcotya", catRoutes);
 
-const server = app.listen(3011, () => console.log("Started on", server.address().port));
+app.use((err, req, res, next) => {
+    res.status(500).send(err);
+})
+
+const server = app.listen(3011, () => console.log("Successfully started on port", server.address().port));
